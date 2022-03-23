023242fdeb553edec77c517e379b3a786d99fd78
Josh bolten <jozhbolt@gmail.com>
2022-03-23 04:10:15 +0100

    added route tables and associations

--- Network.tf
+++ Network.tf
@@ -39,6 +39,27 @@ resource "aws_subnet" "public_subnets" {
  }
 }
 
+/**
+* A route from the public route table out to the internet through the internet
+* gateway.
+*/
+resource "aws_route_table" "public_rt" {
+    vpc_id         = aws_vpc.eks-vpc.id
+    route {
+        cidr_block = "0.0.0.0/0"
+        gateway_id = aws_internet_gateway.main_internet_gateway.id
+    }
+}
+
+/**
+* Associate the public route table with the public subnets.
+*/
+resource "aws_route_table_association" "public" {
+    count          = var.public_subnets_count
+    subnet_id      = element(aws_subnet.public_subnets.*.id, count.index)
+    route_table_id = aws_route_table.public_rt.id
+}
+
 /******************************************************************************
 * Private Subnet 
 *******************************************************************************/
@@ -52,8 +73,8 @@ resource "aws_subnet" "public_subnets" {
 
 
 resource "aws_subnet" "private_subnets" {
-    vpc_id                  = aws_vpc.main_vpc.id
-    cidr_block              = cidrsubnet(aws_vpc.main_vpc.cidr_block, 8, count.index)
+    vpc_id                  = aws_vpc.eks-vpc.id
+    cidr_block              = cidrsubnet(aws_vpc.eks-vpc.cidr_block, 8, count.index)
     availability_zone       = element(var.availability_zones, count.index)
     map_public_ip_on_launch = false
     count                   = var.private_subnets_count
@@ -61,4 +82,27 @@ resource "aws_subnet" "private_subnets" {
     tags = {
         Name                = "${env-prefix}-${count.index *2}.0_${element(var.availability_zones, count.index)}"
  }
+}
+
+
+/**
+* A route from the private route table out to the internet through the NAT  
+* Gateway.
+*/
+
+resource "aws_route_table" "private_rt" {
+    vpc_id             = aws_vpc.eks-vpc.id
+    route {
+        cidr_block     = "0.0.0.0/0"
+        nat_gateway_id = aws_nat_gateway.nat_gateway.id
+ }
+}
+
+/**
+* Associate the private route table with the private subnet.
+*/
+resource "aws_route_table_association" "private" {
+    count          = var.private_subnets_count
+    subnet_id      = element(aws_subnet.private_subnets.*.id, count.index)
+    route_table_id = aws_route_table.private_rt.id
 }
