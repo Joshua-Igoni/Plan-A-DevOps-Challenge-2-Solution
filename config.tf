a2b74d032d942ff93afa71a433b493871336bdf0
Josh bolten <jozhbolt@gmail.com>
2022-03-23 02:18:46 +0100

    configured provider

---
+++ config.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  shared_credentials_files = var.aws_credentials_file
+  profile = var.aws_profile
+  region = var.aws_region
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "aws_credentials_file" {
+  type = list
+}
+variable "aws_profile" {
+  type = list
+}
+variable "aws_region" {
+  type = list
+}
